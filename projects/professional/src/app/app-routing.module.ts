0952629f8f37d68170d03265a9ba796afe74cc3c
Nikhil Janshali <nikhil.janshali@gmail.com>
2023-01-24 23:40:37 +0530

    Visual Changes , image and html

--- projects/professional/src/app/app-routing.module.ts
+++ projects/professional/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './common/component/contact/contact.component';
 import { EducationComponent } from './common/component/education/education.component';
+import { ExpertiesComponent } from './common/component/experties/experties.component';
 import { HomeComponent } from './common/component/home/home.component';
 import { PagenotfoundComponent } from './common/component/pagenotfound/pagenotfound.component';
 import { ProjectComponent } from './common/component/project/project.component';
@@ -14,6 +15,7 @@ const routes: Routes = [
   { path: 'education', component: EducationComponent },
   { path: 'work', component: WorkExperienceComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'expertise', component: ExpertiesComponent },
   { path: '**', component: PagenotfoundComponent }
 ];
 
---
+++ projects/professional/src/app/common/component/experties/experties.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-experties',
+  templateUrl: './experties.component.html',
+  styleUrls: ['./experties.component.scss']
+})
+export class ExpertiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
