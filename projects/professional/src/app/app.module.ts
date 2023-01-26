ce0d4a5a1b5b7c7ac4224f145bbf17f6b822dfb5
Nikhil Janshali <nikhil.janshali@gmail.com>
2023-01-26 21:38:50 +0530

    Update Contact Us Page

--- projects/professional/src/app/app.module.ts
+++ projects/professional/src/app/app.module.ts
@@ -5,16 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/component/header/header.component';
 import { NavigationComponent } from './common/component/navigation/navigation.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ContactComponent } from './common/component/contact/contact.component';
+import { ExpertiesComponent } from './common/component/experties/experties.component';
+import { ProjectComponent } from './common/component/project/project.component';
+import { EducationComponent } from './common/component/education/education.component';
+import { HomeComponent } from './common/component/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    ExpertiesComponent,
+    ProjectComponent,
+    ExpertiesComponent,
+    EducationComponent,
+    HomeComponent,
+    ContactComponent,
     NavigationComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- projects/professional/src/app/common/component/contact/contact.component.ts
+++ projects/professional/src/app/common/component/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  public contactMe!: FormGroup;
+  public submitted: boolean = false;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.contactMe = this.formBuilder.group({
+      fullName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+      mobile: ['', Validators.required],
+      message: ['', Validators.required],
+    });
+
   }
 
+
+  //convenience getter for easy access to form fields
+  get f(): { [key: string]: AbstractControl; } {
+    return this.contactMe.controls;
+  }
+
+
+
+  public onSubmit(event: Event) {
+
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.contactMe.invalid) {
+      return;
+    }
+    else {
+
+      event.preventDefault();
+
+      console.warn(this.contactMe.value);
+    }
+  }
 }
